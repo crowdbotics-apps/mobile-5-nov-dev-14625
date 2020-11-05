268b9d3e9e76bf0b113029b02e6cf84cd500c0bd
Crowdbotics <team@crowdbotics.com>
2020-11-05 12:32:19 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,20 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +28,27 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class GGG(models.Model):
+    "Generated Model"
+    bvcgfdgfdghfytf = models.BigIntegerField()
+    jhguguigjggjhgkjghkjhgk = models.BigIntegerField()
+    bvcbcb = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.CASCADE,
+        related_name="ggg_bvcbcb",
+    )
+    bvhgfhg = models.ManyToManyField(
+        "home.HomePage",
+        related_name="ggg_bvhgfhg",
+    )
+    hgfhfhg = models.GenericIPAddressField(
+        protocol="IPv6",
+        unpack_ipv4=False,
+    )
--- backend/mobile_5_nov_dev_14625/wsgi.py
+++ backend/mobile_5_nov_dev_14625/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_5_nov_dev_14625.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_5_nov_dev_14625.settings")
 
 application = get_wsgi_application()
